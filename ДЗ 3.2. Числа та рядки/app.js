fe49d0a90c2de16732e123629c2c3771117991d7
Andrii Panchenko <panchenko1307@gmail.com>
2024-08-04 22:15:02 +0200

    HW 3.3 done

--- ДЗ 3.2. Числа та рядки/app.js
+++ ДЗ 3.2. Числа та рядки/app.js
@@ -1,5 +1,5 @@
 const string1 = prompt("Enter Value 1");
 const string2 = prompt("Enter Value 2");
 const string3 = prompt("Enter Value 3");
-const strings123 = `${string1}, ${string2}, ${string3}`;
+const strings123 = `You have entered: ${string1}, ${string2}, ${string3}`;
 alert(strings123);
---
+++ ДЗ 3.3. Розкласти п'ятизначне число/app.js
@@ -0,0 +1,3 @@
+const number = prompt('Enter a 5 digit number');
+const output = `${number[0]} ${number[1]} ${number[2]} ${number[3]} ${number[4]}`;
+alert(output);
