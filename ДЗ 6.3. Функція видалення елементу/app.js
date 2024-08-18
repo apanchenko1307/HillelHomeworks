d175e88c33370df5e6ff7e616eadbc4afa21c287
Andrii Panchenko <panchenko1307@gmail.com>
2024-08-18 19:50:45 +0200

    6.1 added

--- ДЗ 6.3. Функція видалення елементу/app.js
+++ ДЗ 6.3. Функція видалення елементу/app.js
@@ -4,12 +4,13 @@
 // removeElement(array,4);
 // console.log(array); // Результат: [1, 3, 6, 2, 5, 7]
 
-const array = [1, 3, 4, 6, 2, 5, 7];
+const array = [1, 3, 4, 4, 6, 2, 5, 7];
 
 function removeElement(array, item) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] === item) {
             array.splice(i, 1);
+            i--;
         }
     }
 }
