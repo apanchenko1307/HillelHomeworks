469896a658d7ad8a32fa1ee641984ceb419f66d0
Andrii Panchenko <panchenko1307@gmail.com>
2024-10-05 18:38:01 +0200

    17.3

---
+++ ДЗ 17.3. Клас "Банківський рахунок"/app.js
@@ -0,0 +1,34 @@
+// Створіть клас BankAccount, який буде представляти банківський рахунок. 
+// Додайте властивість балансу та методи для внесення та зняття грошей.
+
+class BankAccount {
+    #balance;
+
+    constructor(balance) {
+        this.#balance = balance;
+    }
+
+    getBalance() {
+        return this.#balance;
+    }
+
+    deposit(sum) {
+        this.#balance += sum;
+    }
+
+    withdraw(sum) {
+        this.#balance -= sum;
+    }
+}
+
+const account1 = new BankAccount(1000);
+
+console.log(account1.getBalance()); // 1000
+
+account1.deposit(500);
+
+console.log(account1.getBalance()); // 1500
+
+account1.withdraw(200);
+
+console.log(account1.getBalance()); // 1300
