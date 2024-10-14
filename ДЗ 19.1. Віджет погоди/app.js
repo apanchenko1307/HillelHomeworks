63ee82d8c9ea109190711efd46e6c8c566e13fec
Andrii Panchenko <panchenko1307@gmail.com>
2024-10-14 20:52:58 +0200

    19.1 refresh btn

--- ДЗ 19.1. Віджет погоди/app.js
+++ ДЗ 19.1. Віджет погоди/app.js
@@ -1,8 +1,14 @@
 const apiKey = '80b7e1fb0cd3247680051c0f3ef31ad9';
 let cityName = prompt('Введіть назву міста');
 
+document.querySelector('.refresh-btn').addEventListener('click', function() {
+    displayWeather(cityName, apiKey);
+});
 
-fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
+displayWeather(cityName, apiKey)
+
+function displayWeather(cityName, apiKey) {
+    fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
         if (data.length === 0) {
@@ -14,35 +20,35 @@ fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=1&appid=
         .then(result => {
             document.querySelector('.weather-widget').classList.remove('hidden');
             document.querySelector('.date').textContent = getDate(result.dt, result.timezone);
-            document.querySelector('.time').textContent = getTime(result.dt, result.timezone);
+            document.querySelector('.time').firstElementChild.textContent = getTime(result.dt, result.timezone);
             document.querySelector('.temperature').firstElementChild.textContent = Math.round(result.main.temp * 2) / 2;
             document.querySelector('.weather-condition').textContent = result.weather[0].main;
             document.querySelector('.humidity').firstElementChild.textContent = result.main.humidity;
             document.querySelector('.pressure').firstElementChild.textContent = result.main.pressure;
             document.querySelector('.wind').firstElementChild.textContent = result.wind.speed;
             document.querySelector('.weather-icon').src = `https://openweathermap.org/img/wn/${result.weather[0].icon}@2x.png`;
-
+            
         })
     })
     .catch(error => {
         document.querySelector('.weather-widget').classList.add('hidden');
         console.error('Помилка:', error);
     });
+}
     
-    function getDate(dt, timezone) {
-        const localTime = new Date((dt + timezone) * 1000);
-        const day = localTime.getUTCDate();
-        const month = localTime.getUTCMonth() + 1;
-        const year = localTime.getUTCFullYear();
+function getDate(dt, timezone) {
+    const localTime = new Date((dt + timezone) * 1000);
+    const day = localTime.getDate();
+    const month = localTime.getMonth() + 1;
+    const year = localTime.getFullYear();
+    
+    return `${day.toString().padStart(2, '0')}.${month.toString().padStart(2, '0')}.${year}`;
+}
 
-        return `${day}.${month}.${year}`;
-    }
-
-    function getTime(dt, timezone) {
-        const localTime = new Date((dt + timezone) * 1000);
-
-        const hours = localTime.getUTCHours().toString().padStart(2, '0');
-        const minutes = localTime.getUTCMinutes().toString().padStart(2, '0');
-
-        return `${hours}:${minutes}`;
-    }
+function getTime(dt, timezone) {
+    const localTime = new Date((dt + timezone) * 1000);
+    const hours = localTime.getHours().toString().padStart(2, '0');
+    const minutes = localTime.getMinutes().toString().padStart(2, '0');
+    
+    return `${hours}:${minutes}`;
+}
