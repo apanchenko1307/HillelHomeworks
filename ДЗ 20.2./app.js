4a61c8cd2cd1a47940504bf108f103e05c8c0a57
Andrii Panchenko <panchenko1307@gmail.com>
2024-10-18 00:01:05 +0200

    20.2

---
+++ ДЗ 20.2./app.js
@@ -0,0 +1,20 @@
+const myDateOfBirth = '28.01.1980';
+const formattedDate = moment(myDateOfBirth, 'DD.MM.YYYY').format('MMMM Do YYYY');
+
+const p = document.querySelector('.myB-day');
+p.textContent = `I was born on ${formattedDate}.`;
+
+const inputUserBday = document.querySelector('#userB-day');
+inputUserBday.addEventListener('input', function(event) {
+    const message = document.querySelector('[role="alert"]');
+    if (!/^(0[1-9]|[12][0-9]|3[01])\.(0[1-9]|1[0-2])\.(\d{4})$/.test(event.target.value)) {
+        message.classList.add("alert", "alert-danger");
+        message.textContent = 'The date entered is incorrect'
+    } else {
+        const userFormattedDate = moment(event.target.value, 'DD.MM.YYYY').format('MMMM Do YYYY');
+        message.className = '';
+        message.classList.add("alert", "alert-success");
+        message.textContent = `You were born on ${userFormattedDate}`;
+    }
+
+});
